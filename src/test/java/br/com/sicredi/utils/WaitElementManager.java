dfea004a85a4f951bc8a753ea4184433a57c6627
dfernandos <danieldfs18@hotmail.com>
2020-06-05 08:26:48 +0800

    remove unused methods and update timeout variable

--- src/test/java/br/com/sicredi/utils/WaitElementManager.java
+++ src/test/java/br/com/sicredi/utils/WaitElementManager.java
@@ -2,32 +2,16 @@ package br.com.sicredi.utils;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitElementManager {
 
-    private static final int TIMEOUT_SEC = 15;
+    private static final int TIMEOUT_SECONDS = 10;
 
     public static void visibilityOfElementLocated(WebDriver driver, By by) {
-        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT_SEC);
+        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT_SECONDS);
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-    }
-
-    public static void visibilityOf(WebDriver driver, WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT_SEC);
-        wait.until(ExpectedConditions.visibilityOf(element));
-    }
-
-    public static void elementToBeClickable(WebDriver driver, By by) {
-        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT_SEC);
-        wait.until(ExpectedConditions.elementToBeClickable(by));
-    }
-
-    public static void elementToBeClickable(WebDriver driver, WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT_SEC);
-        wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
 }
